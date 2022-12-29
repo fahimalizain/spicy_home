402e5f7056a0272c72420401bc9e6d2720419619
Fahim Ali Zain <fahimalizain@gmail.com>
2022-12-29 08:10:36 +0530

    feat: RMS Order DocType

---
+++ spicy_erpnext/spicy_erpnext/rms_pos/doctype/rms_order/test_rms_order.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Fahim Ali Zain and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRMSOrder(FrappeTestCase):
+	pass
