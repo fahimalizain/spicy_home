1e0b563aaeb99eaac789711c45ad0a29ee90f4ca
Fahim Ali Zain <fahimalizain@gmail.com>
2022-12-29 07:41:10 +0530

    feat: RMS Item Doctype

---
+++ spicy_erpnext/spicy_erpnext/rms_pos/doctype/rms_item/rms_item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Fahim Ali Zain and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('RMS Item', {
+	// refresh: function(frm) {
+
+	// }
+});
