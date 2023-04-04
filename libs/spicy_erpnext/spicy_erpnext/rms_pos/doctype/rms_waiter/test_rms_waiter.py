61c3bfae1528842f0dc517e7ab212ef7c49757fe
Fahim Ali Zain <fahimalizain@gmail.com>
2023-04-04 11:47:29 +0530

    feat: RMS Waiter DocType

---
+++ libs/spicy_erpnext/spicy_erpnext/rms_pos/doctype/rms_waiter/test_rms_waiter.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Fahim Ali Zain and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRMSWaiter(FrappeTestCase):
+	pass
