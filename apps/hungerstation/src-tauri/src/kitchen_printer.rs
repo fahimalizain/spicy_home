a1a5aa74de0909c4c21420cb40ce36c1b01ca58c
Fahim Ali Zain <fahimalizain@gmail.com>
2023-10-12 16:13:27 +0530

    fix: Update Print Layouts

--- apps/hungerstation/src-tauri/src/kitchen_printer.rs
+++ apps/hungerstation/src-tauri/src/kitchen_printer.rs
@@ -4,55 +4,95 @@ use escposify::printer::Printer;
 use crate::types::Order;
 use crate::types::PrinterSettings;
 
+// 42 chars per line
+
 pub fn send(settings: PrinterSettings, order: Order) -> Result<(), std::io::Error> {
     let printer = Network::new("192.168.1.100", 9100)?;
     let mut printer = Printer::new(printer, None, None);
     printer
-        .chain_font("B")?
+        .chain_font("A")?
         .chain_style("B")?
         .chain_align("CT")?
+        .chain_size(2, 2)?
         .chain_text("SpicyHome Restaurant")?
-        .chain_font("A")?
+        .chain_size(0, 0)?
+        .chain_style("NORMAL")?
+        .chain_text("Hafsah Bint Omar Street, Rawdah, Riyadh")?
+        .chain_feed(1)?
         .chain_text(&"Hungerstation Order")?
-        .chain_feed(2)?;
+        .chain_feed(1)?;
 
     printer
         .chain_align("LT")?
         .chain_text(&("Order #".to_owned() + &order.order_id))?
         .chain_text(&("Time: ".to_owned() + &order.placed_timestamp))?;
 
-
-    // Column Headers
-    let (mut printer, settings) = draw_line(printer, settings)?;
-    printer
-        .chain_align("LT")?
-        .chain_text(&("Item".to_owned() + &" ".repeat(20) + "Qty" + &"_".repeat(10) + "Rate"))?;
-    let (mut printer, settings) = draw_line(printer, settings)?;
-
-    // Loop through order items
-    for item in order.items {
-        printer.chain_align("LT")?.chain_text(
-            &(get_first_15_chars(&item.name)
-                + &" ".repeat(5)
-                + &item.quantity.to_string()
-                + &" ".repeat(8)
-                + &item.unit_price.to_string()),
-        )?;
+    // Item Table
+    let item_table_lines = format_order_table(&order, &settings);
+    for (_, line) in item_table_lines.iter().enumerate() {
+        printer.println(line);
     }
-
-    let (mut printer, _settings) = draw_line(printer, settings)?;
 
     // Order Totals
     printer
         .chain_align("RT")?
-        .chain_text(&("Total: ".to_owned() + &order.order_value.to_string() + "SAR"))?
-        .chain_feed(2)?
-        .chain_text("Thank you for your order")?
-        .chain_text("Please come again")?
-        .chain_text("Powered by SpicyHome")?
+        .chain_size(2, 2)?
+        .chain_text(&("Total: ".to_owned() + &order.order_value.to_string() + " SAR"))?
+        .chain_size(0, 0)?
+        .chain_feed(2)?;
+
+    // Footer
+    printer
+        .chain_align("CT")?
+        .chain_text("Thank you for your Order!")?
+        .chain_text("Contact Us: +966112357926")?
         .chain_text("https://spicyhomeksa.com")?;
 
-    printer.chain_feed(1)?.chain_cut(false)?.flush()
+    printer
+        .chain_feed(1)?
+        .chain_cut(false)?
+        .chain_hwreset()?
+        .flush()
+}
+
+fn format_order_table(order: &Order, settings: &PrinterSettings) -> Vec<String> {
+    // Assuming the widths (including spaces) are:
+    // Item (20 chars), Qty (4 chars), Rate (6 chars) and Amount (7 chars)
+    const ITEM_WIDTH: usize = 20;
+    const QTY_WIDTH: usize = 4;
+    const RATE_WIDTH: usize = 6;
+    const AMOUNT_WIDTH: usize = 7;
+
+    let mut lines: Vec<String> = Vec::new();
+
+    // Add
+    lines.push("-".repeat(settings.width.into()));
+
+    // Add the table headers
+    lines.push(format!(
+        "{:<20} {:<4} {:<6} {:<7}",
+        "Item", "Qty", "Rate", "Amount"
+    ));
+
+    lines.push("-".repeat(settings.width.into()));
+
+    for item in &order.items {
+        let mut item_name_parts = item.name.clone();
+        while item_name_parts.len() > ITEM_WIDTH {
+            // Extract the first 20 characters
+            let part = &item_name_parts[..ITEM_WIDTH];
+            lines.push(format!("{:<20}", part));
+            item_name_parts = item_name_parts[ITEM_WIDTH..].to_string();
+        }
+        let amount = item.quantity as f64 * item.unit_price;
+        lines.push(format!(
+            "{:<20} {:<4} {:>6.2} {:>7.2}",
+            item_name_parts, item.quantity, item.unit_price, amount
+        ));
+    }
+    lines.push("-".repeat(settings.width.into()));
+
+    lines
 }
 
 fn get_first_15_chars(input: &str) -> String {
--- apps/hungerstation/swr/kitchen-print/service.ts
+++ apps/hungerstation/swr/kitchen-print/service.ts
@@ -2,11 +2,18 @@
 // import NetworkPrinter from '@node-escpos/network-adapter';
 import { KitchenPrintRequest, KitchenPrintResponse } from './types';
 import { invoke } from '@tauri-apps/api/tauri';
+import { parseISO, format } from 'date-fns';
 
 export async function kitchenPrint(
   params: KitchenPrintRequest
 ): Promise<KitchenPrintResponse> {
   return invoke<KitchenPrintResponse>('kitchen_print', {
-    order: JSON.stringify(params.order),
+    order: JSON.stringify({
+      ...params.order,
+      placedTimestamp: format(
+        parseISO(params.order.placedTimestamp),
+        'dd/MM/yyyy h:mm aa'
+      ),
+    }),
   });
 }
